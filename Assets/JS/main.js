8a38d63e13dae9fb3c93c45a37be190c43c541f8
Tommy Hsieh <thetommyhsieh@gmail.com>
2020-05-02 20:51:18 +0800

    Add eventListener to email.

---
+++ Assets/JS/main.js
@@ -0,0 +1,8 @@
+const email = document.querySelector('#email');
+email.addEventListener('input', function(event) {
+	if (email.validity.typeMismatch) {
+		email.setCustomValidity('I am expecting an e-mail address!');
+	} else {
+		email.setCustomValidity('');
+	}
+});
